93311d5d4f062977b6a3d3e0409c91f23200ac73
Wang Hao <eric.3126@hotmail.com>
2014-01-02 14:36:25 +0800

    hiapk bug fixed

--- src/PolySpider/spiders/hiapk.py
+++ src/PolySpider/spiders/hiapk.py
@@ -8,14 +8,14 @@ from PolySpider import CommonUtils
 import urllib2
 class AppStarSpider(CrawlSpider):
 	name = "hiapk"
-	allowed_domains = ["hiapk.com"]
+	allowed_domains = ["apk.hiapk.com"]
 	start_urls = [
-                "http://apk.hiapk.com/apps"
+                "http://apk.hiapk.com"
 	]
-       # rules = [Rule(SgmlLinkExtractor(allow=("http\://apk\.hiapk\.com/html/[0-9]*/[0-9]*/[0-9]*\.html", )), callback='parse_app'),]
         rules = [
-		Rule(SgmlLinkExtractor(allow=('/[0-9]*/[0-9]*/[0-9]*\.html', )),callback='parse_app',follow=True)
-	]
+            Rule(SgmlLinkExtractor(allow=("apk\.hiapk\.com/html/[0-9]*/[0-9]*/[0-9]*\.html", )),callback='parse_app'),
+            Rule(SgmlLinkExtractor(allow=("apk\.hiapk\.com", ),deny=("down\.apk\.hiapk\.com","apk\.hiapk\.com/Download\.aspx", )), follow = True),
+        ]
 	def parse_app(self, response):	
 
             sel = Selector(response)
@@ -40,9 +40,14 @@ class AppStarSpider(CrawlSpider):
             item['last_update'] =   sel.xpath('//*[@id="ctl00_AndroidMaster_Content_Apk_SoftPublishTime"]/text()').extract()[0]
             item['description'] =  sel.xpath('//*[@id="ctl00_AndroidMaster_Content_Apk_Description"]/text()').extract()[0]
             item['apk_size'] = sel.xpath('//*[@id="ctl00_AndroidMaster_Content_Apk_SoftSize"]/text()').extract()[0]
-            item['rating_count'] = sel.xpath('//*[@id="ctl00_AndroidMaster_Content_Soft_StarProportion"]/div[2]/div[2]/div[3]/text()').extract()[0][:-3]
-            item['rating_star'] = sel.xpath('//*[@id="main"]/div/div/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/@class').extract()[0][21:-2]
-            if "half" in item['rating_star']: item['rating_star'] = item['rating_star'][0] + ".5"
+            rating_star = sel.xpath('//*[@id="main"]/div/div/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/@class').extract()[0]
+            if len(rating_star) < 21:
+                item['rating_star'] = "0"
+                item['rating_count'] = "0"
+            else:
+                item['rating_star'] = rating_star[21:-2] if len(rating_star) > 21 else "0"
+                if "half" in item['rating_star']: item['rating_star'] = item['rating_star'][0] + ".5"
+                item['rating_count'] = sel.xpath('//*[@id="ctl00_AndroidMaster_Content_Soft_StarProportion"]/div[2]/div[2]/div[3]/text()').extract()[0][:-3]
             #获取图片地址，通过空格来分割多张图片
             imgs =  sel.xpath('//*[@id="main"]/div/div/div[1]/div[1]/div[2]/div[4]/div[3]/ul/li/a/@href').extract()
             imgs_url = ""
