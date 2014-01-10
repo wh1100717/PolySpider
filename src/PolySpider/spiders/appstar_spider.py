b797a82ad552bae985b00362fb70e47741758857
zhaoqing <530030134@qq.com>
2014-01-10 17:32:10 +0800

    bug fixed appstar

--- src/PolySpider/spiders/appstar_spider.py
+++ src/PolySpider/spiders/appstar_spider.py
@@ -46,8 +46,8 @@ class AppStarSpider(BaseSpider):
                 item['rating_count'] = sel.xpath('//*[@id="appDetail"]/li[2]/span[1]/text()').extract()[0][:-3]
                 item['category'] = sel.xpath('//*[@id="appDetail"]/li[1]/text()').extract()[1][6:]
                 item['android_version'] = sel.xpath('//*[@id="appDetail"]/li[1]/text()').extract()[4][5:]
-                item['download_times'] = CommonUtil.download_time_normalize(sel.xpath('//*[@id="appDetail"]/li[2]/text()').extract()[1][5:-1])
-                author=sel.xpath('//*[@id="appDetail"]/li[2]/text()').extract()
+                item['download_times'] = CommonUtil.download_time_normalize(sel.xpath('//*[@id="appDetail"]/li[2]/text()[2]').extract()[0][5:-1])
+                author=sel.xpath('//*[@id="appDetail"]/li[2]/text()[3]').extract()
                 if len(author)==0:
                     author = ''
                 else:
